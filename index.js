720d2b010804f2bd8b028930f1f26b67ed5a1fc5
Jon Börjesson <jon@spuun.se>
2020-03-03 13:04:28 +0100

    Publish data to mqtt

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
-const AnimusHeart = require('jons-animusheart')
+const mqtt = require('mqtt')
 
+const AnimusHeart = require('jons-animusheart')
 const Logger = require('./logger')
 
 const apiKey = process.env.ANIMUS_APIKEY
 const wsProtocol = 'AHauth'
 
 
-const logger = new Logger(Logger.Level.debug)
+const logger = new Logger(Logger.Level.info)
 const cacheOpts = { ttl: 0, logger }
 const cache = new AnimusHeart.Cache.FileCache('./cache', cacheOpts)
 const heartOpts = { cache, logger }
@@ -16,9 +17,26 @@ const heart = new AnimusHeart(process.env.ANIMUS_IP,
                               heartOpts)
 logger.info('Heart', heart)
 
+function heartEvent2MqttTopic(d) {
+  const [deviceId, funcId] = d.functionId.split(':')
+  return `heart/${deviceId}/${funcId}/${d.propertyName}`
+}
+
 const init = async () => {
   try {
-   heart.events.subscribe(d => logger.debug("[DATA]", d))
+    const client = mqtt.connect(process.env.MQTT_URL, {
+      clientid: 'heart2mqtt',
+      reconnectPeriod: 3*1000,
+      connectTimeout: 10*1000
+    })
+    client.on('connect', _ => logger.info("[MQTT] Connected"))
+    client.on('error', e => logger.error('[MQTT]', e))
+    client.on('disconnect', _ => logger.info('[MQTT] Disconnected'))
+    client.on('reconnect', _ => logger.info('[MQTT] Reconnecting'))
+    heart.events.subscribe(d => {
+      logger.info("[DATA]", d)
+      client.publish(heartEvent2MqttTopic(d), d.data)
+    })
   } catch (ex) {
     logger.error('Error init', ex)
   }
